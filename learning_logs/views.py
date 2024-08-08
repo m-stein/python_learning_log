0452b7b287ac8a5fe5672d495662408a2bd0f5b8
Martin Stein <martin.stein.2023@gmail.com>
2024-08-08 21:32:47 +0200

    sub-pages for topics and their log entries

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from .models import Topic
+
 
 def index(request):
     return render(request, template_name='learning_logs/index.html')
+
+
+def topics(request):
+    return render(request, template_name='learning_logs/topics.html',
+                  context={'topics': Topic.objects.order_by('date_added')})
+
+
+def topic(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+    return render(request, template_name='learning_logs/topic.html',
+                  context={'topic': topic, 'entries': topic.entry_set.order_by('date_added')})

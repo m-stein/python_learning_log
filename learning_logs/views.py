8bc9668c64d75a82bb1b3b55fd37471469afda84
Martin Stein <martin.stein.2023@gmail.com>
2024-08-17 21:02:17 +0200

    restrict user access to only those topics they own

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,29 +1,43 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
+
+
+def _assert_allowed_to_access_topic(request, topic):
+    if topic.owner != request.user:
+        raise Http404
 
 
 def index(request):
     return render(request, template_name='learning_logs/index.html')
 
 
+@login_required
 def topics(request):
     return render(request, template_name='learning_logs/topics.html',
-                  context={'topics': Topic.objects.order_by('text')})
+                  context={'topics': Topic.objects.filter(owner=request.user).order_by('text')})
 
 
+@login_required
 def topic(request, topic_id):
     topic = Topic.objects.get(id=topic_id)
+    _assert_allowed_to_access_topic(request, topic)
+
     return render(request, template_name='learning_logs/topic.html',
                   context={'topic': topic, 'entries': topic.entry_set.order_by('-date_added')})
 
 
+@login_required
 def new_topic(request):
     # if user data was submitted, process the data and return to other page
     if request.method == 'POST':
         form = TopicForm(data=request.POST)
         if form.is_valid():
-            form.save()
+            topic = form.save(commit=False)
+            topic.owner = request.user
+            topic.save()
             return redirect('learning_logs:topics')
         else:
             print("Warning: form not valid")
@@ -32,8 +46,10 @@ def new_topic(request):
     return render(request, template_name='learning_logs/new_topic.html', context={'form': TopicForm()})
 
 
+@login_required
 def new_entry(request, topic_id):
     topic = Topic.objects.get(id=topic_id)
+    _assert_allowed_to_access_topic(request, topic)
 
     # if user data was submitted, process the data and return to other page
     if request.method == 'POST':
@@ -51,8 +67,10 @@ def new_entry(request, topic_id):
                   context={'topic': topic, 'form': EntryForm()})
 
 
+@login_required
 def edit_entry(request, entry_id):
     entry = Entry.objects.get(id=entry_id)
+    _assert_allowed_to_access_topic(request, entry.topic)
 
     # if user data was submitted, process the data and return to other page
     if request.method == 'POST':
